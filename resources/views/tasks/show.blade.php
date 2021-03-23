7d3e066099b4ee5d83b17090ceb1e40266ec3674
yoshitaka.shikina <yoshitaka.shikina0216@gmail.com>
2021-03-23 10:15:54 +0000

    kadai-tasklist

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,10 +2,23 @@
 
 @section('content')
 
-{{-- タスク編集ページへのリンク --}}
+<h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+
+     {{-- タスク編集ページへのリンク --}}
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
 
-{{-- メッセージ削除フォーム --}}
+    {{-- タスク削除フォーム --}}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
